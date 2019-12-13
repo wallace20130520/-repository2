701e0299064cfd52ebd349faceccbfd66e79e11d
I323130 <wallace.zhao@sap.com>
2019-12-13 14:40:00 +0800

    no
    no

--- js-storefront/MoltonBrown-ui/src/app/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
-import { PATH } from "./app.constant";
-import { map } from 'rxjs/operators';
-@Injectable()
-export class AppService  {
- 
-}
