e9f914d86604dc0803a2ba4b45bbdd41889091e5
I323130 <wallace.zhao@sap.com>
2020-05-31 00:26:41 +0800

    123123123123123123123123123123123123

---
+++ core-customize/custom/ccv2cloudhotfolder/gensrc/de/hybris/platform/cloud/azure/hotfolder/constants/GeneratedAzurecloudhotfolderConstants.java
@@ -0,0 +1,31 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 30, 2020, 9:17:01 PM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.platform.cloud.azure.hotfolder.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedAzurecloudhotfolderConstants
+{
+	public static final String EXTENSIONNAME = "azurecloudhotfolder";
+	
+	protected GeneratedAzurecloudhotfolderConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
