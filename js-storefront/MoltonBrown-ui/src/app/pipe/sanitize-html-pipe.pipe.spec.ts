0da6caff019cfca50210b4be52261a1b9d78a6fd
I323130 <wallace.zhao@sap.com>
2020-08-19 11:40:33 +0800

    ECP-503

--- js-storefront/MoltonBrown-ui/src/app/pipe/sanitize-html-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SanitizeHtmlPipePipe } from './sanitize-html-pipe.pipe';
-
-describe('SanitizeHtmlPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SanitizeHtmlPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
