be97284d6f3775a3bed88f6481bdd50caed7d180
I323130 <wallace.zhao@sap.com>
2020-08-26 08:48:16 +0800

    082601

---
+++ core-customize/xyformsstorefrontcommons/acceleratoraddon/web/src/de/hybris/platform/xyformsstorefrontcommons/controllers/XyformsstorefrontcommonsControllerConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.xyformsstorefrontcommons.controllers;
+
+/**
+ */
+public interface XyformsstorefrontcommonsControllerConstants
+{
+	// implement here controller constants used by this extension
+}
